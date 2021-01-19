47caeec02e641278025c04af3807dacebfe7c075
Olof Bjarnason <olof.bjarnason@gmail.com>
2021-01-19 18:49:37 +0100

    Better initial tests

--- cpp/gtest_all.cpp
+++ cpp/gtest_all.cpp
@@ -3,8 +3,12 @@
 
 const std::string toRobbersLanguage(std::string const & english);
 
-TEST(RobbersLanguage, TwoLetters) {
-    EXPECT_EQ("lole", toRobbersLanguage("le"));
+TEST(RobbersLanguage, Test1) {
+    EXPECT_EQ("lol", toRobbersLanguage("l"));
 }
 
+
+TEST(RobbersLanguage, Test2) {
+    EXPECT_EQ("aa", toRobbersLanguage("aa"));
+}
 
