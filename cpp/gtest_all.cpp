7831f60e214b3c925ac3610258fa26e459e2e3e2
Olof Bjarnason <olof.bjarnason@gmail.com>
2021-01-25 09:04:06 +0000

    Remove "tennis" from build output

--- cpp/gtest_all.cpp
+++ cpp/gtest_all.cpp
@@ -6,9 +6,3 @@ const std::string toRobbersLanguage(std::string const & english);
 TEST(RobbersLanguage, Test1) {
     EXPECT_EQ("lol", toRobbersLanguage("l"));
 }
-
-
-TEST(RobbersLanguage, Test2) {
-    EXPECT_EQ("aa", toRobbersLanguage("aa"));
-}
-
